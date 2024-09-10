cd389856a1eb348853c66f72ff2b4145b1a2eca1
MrMelnyk <mrmelnyk.dev@gmail.com>
2024-09-10 11:04:38 -0600

    Updates to the script.

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ let scrollLeft;
     startX = e.pageX - slider.offsetLeft;
     scrollLeft = slider.scrollLeft;
   });
-
    
   slider.addEventListener('mouseleave', () => {
     isDown = false;
@@ -36,7 +35,7 @@ const showroom = document.querySelector('.showpic');
 let zoomed = false;
 
   showroom.addEventListener('click', (e) => {
-    if(!zoomed) return;
+    //if(!zoomed) return;
     e.preventDefault();
     showroom.classList.remove('zoom');
     zoomed = false;
