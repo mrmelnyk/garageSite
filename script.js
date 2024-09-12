9c3cb5cb9e89e5b2d499bc3b63c1b7bd97fb8cf2
MrMelnyk <mrmelnyk.dev@gmail.com>
2024-09-12 11:08:29 -0600

    Changes to try and run more than one function at a time.

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const showroom = document.querySelector('.showpic');
 let zoomed = false;
 
-  showroom.addEventListener('click', (e) => {
-    if(!zoomed) return;
-    e.preventDefault();
-    showroom.classList.remove('zoom');
-    zoomed = false;
-  });
+  
 
-  showroom.addEventListener('click', () => {
-    showroom.classList.add('zoom');
-    zoomed = true;
-  });
+    showroom.addEventListener('click', () => {
+        showroom.classList.add('zoom');
+        zoomed = true;
+    }); 
+
+//showroom.addEventListener('click', (e) => {
+//  if(!zoomed) return;
+//  e.preventDefault();
+//  showroom.classList.remove('zoom');
+//  zoomed = false;
+//});
